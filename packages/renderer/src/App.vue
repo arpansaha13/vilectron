3ee71c376b000a4331c805b4b53eded2c79fc428
lemontreejs <arpansaha1300@gmail.com>
2022-03-15 13:52:24 +0530

    feat: added icons

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <img alt="Vue logo" class="mx-auto" src="/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="w-screen h-screen flex flex-col items-center justify-center dark:text-gray-50 dark:bg-gray-800">
+    <div class="mx-auto max-w-lg grid grid-cols-3 gap-8">
+      <img v-for="img in imgs" :key="img.src" :src="img.src" :alt="img.alt" />
+    </div>
+    <HelloWorld class="mt-4" msg="Hello Vue 3 + Vite" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,6 +17,14 @@ export default defineComponent({
   },
   setup() {
     onMounted(() => { console.log('mounted') })
+
+    const imgs = [
+      { src: '/electron.png', alt: 'Electron logo', },
+      { src: '/vue.png', alt: 'Vue logo', },
+      { src: '/vite.svg', alt: 'Vite logo', },
+    ]
+
+    return { imgs }
   },
 })
 </script>
--- packages/renderer/src/components/HelloWorld.vue
+++ packages/renderer/src/components/HelloWorld.vue
@@ -1,32 +1,34 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="text-center">
+    <h1>{{ msg }}</h1>
 
-  <p class="mt-4">
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+    <p class="mt-4">
+      Recommended IDE setup:
+      <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+      +
+      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    </p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+    <p>
+      <a href="https://www.electronjs.org/docs/latest" target="_blank">Electron Documentation</a>
+      |
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a>
+      |
+      <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    </p>
 
-  <button
-    type="button"
-    class="my-4 inline-flex items-center px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-emerald-600 hover:bg-emerald-700"
-    @click="count++"
-  >
-    count is: {{ count }}
-  </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button
+      type="button"
+      class="my-4 inline-flex items-center px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-emerald-600 hover:bg-emerald-700"
+      @click="count++"
+    >
+      Count is: {{ count }}
+    </button>
+    <p>
+      Edit
+      <code class="text-sm">components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
 </template>
 
 <style scoped>
