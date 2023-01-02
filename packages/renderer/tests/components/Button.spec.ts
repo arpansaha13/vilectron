178bc43b12e2fa160fd0ebf4af9f26418a929700
arpansaha13 <arpansaha1300@gmail.com>
2023-01-02 09:55:35 +0530

    chore: update deps and general maintenance

--- packages/renderer/tests/components/Button.spec.ts
+++ packages/renderer/tests/components/Button.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import Button from '~/components/Button.vue'
 
